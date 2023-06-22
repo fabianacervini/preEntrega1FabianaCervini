b6c3fab1aa4608b53485dd081418f093d4feba45
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-06-21 23:14:38 -0300

    comiteo de variable

---
+++ main.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Por favor estimado, escriba su nombre");
+alert(`Gracias por su información ${nombre}`);
+let apellido = prompt("Por favor, ahora escriba su apellido");
+alert(`Gracias ${nombre} ${apellido}, eso ya lo sabía, sólo era una formalidad`);
+let pregunta = prompt("Cómo te fue en tu día?");
+alert("No soy la matrix, pero espero que hayas respondido que bien");
+let preguntaDos = prompt("Por ahora me despido, podemos seguir por whatsapp, tipee OK para salir");
+alert("Nos vemos en whatsapp, hermoso <3");
